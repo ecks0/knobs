a9117d5d2ed1d2791290d75617c079e17d1239a0
eckso <eckso@eckso.io>
2022-01-02 21:39:00 -0500

    dynamically size the table header seps, revert to - as sep

--- src/util/format.rs
+++ src/util/format.rs
@@ -38,24 +38,44 @@ pub(crate) fn power(p: Power) -> String {
 }
 
 #[derive(Debug)]
-pub(crate) struct Table(ct::Table);
+pub struct Table<'a> {
+    header: &'a [&'a str],
+    rows: Vec<Vec<String>>,
+}
 
-impl Table {
-    pub(crate) fn new(header: &[&str]) -> Self {
-        let mut tab = ct::Table::new();
-        tab.load_preset(ct::presets::NOTHING);
-        tab.set_header(header);
-        tab.add_row(header.iter().map(|h| "─".repeat(h.len())).collect::<Vec<String>>());
-        Self(tab)
+impl<'a> Table<'a> {
+    const LINE: &'static str = "-";
+
+    pub(crate) fn new(header: &'a [&'a str]) -> Self {
+        let rows = vec![];
+        Self { header, rows }
     }
 
     pub(crate) fn row<S: Display>(&mut self, row: &[S]) {
-        self.0.add_row(row);
+        let row = row.iter().map(ToString::to_string).collect();
+        self.rows.push(row);
     }
 }
 
-impl std::fmt::Display for Table {
+impl<'a> Display for Table<'a> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        writeln!(f, "{}", self.0)
+        let seps: Vec<_> = self
+            .header
+            .iter()
+            .enumerate()
+            .map(|(i, h)| {
+                let h = h.chars().count();
+                let c = self.rows.iter().fold(h, |a, v| v[i].chars().count().max(a));
+                Self::LINE.repeat(c)
+            })
+            .collect();
+        let mut tab = ct::Table::new();
+        tab.load_preset(ct::presets::NOTHING);
+        tab.set_header(self.header);
+        tab.add_row(seps);
+        for row in &self.rows {
+            tab.add_row(row);
+        }
+        writeln!(f, "{}", tab.to_string())
     }
 }
